6cfe7f2e85b4fd51e59cb4e694acffa33ca29c69
HassanKhan123 <hk2602809@gmail.com>
2022-01-20 14:55:28 +0500

    express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("HEllooo");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+        <div>
+            <h1>Hi!</h1>
+        </div>
+    `);
+});
+
+app.listen(3000, () => {
+  console.log("SERVER STARTED");
+});
