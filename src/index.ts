04ce427e680ae87b684e5df34e7433dc29e837cb
HassanKhan123 <hk2602809@gmail.com>
2022-01-20 15:07:21 +0500

    login route added

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import bodyParser from "body-parser";
+
+import { router } from "./routes/loginRoutes";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(`
-        <div>
-            <h1>Hi!</h1>
-        </div>
-    `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log("SERVER STARTED");
